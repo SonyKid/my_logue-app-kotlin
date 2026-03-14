70206ebc6d5f90b2b81c34ff120f203f31d874cb
spencehouse <mc@spence.house>
2026-03-14 16:08:02 -0400

    Update version to 0.5.0
    - Increment `versionCode` to 9.
    - Update `versionName` to `0.5.0` in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.spencehouse.logue"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.7"
+        versionCode = 9
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
